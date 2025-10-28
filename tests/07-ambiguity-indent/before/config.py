75e8fa9298bb38c105b06f42899bcb2fd1e2e160
junovhs <junovhs@gmail.com>
2025-10-28 15:46:47 -0700

    intial scaffold commit

--- tests/07-ambiguity-indent/before/config.py
+++
@@ -1,9 +0,0 @@
-def configure_alpha():
-    # First occurrence
-    print("Enabling feature_xyz...")
-    print("Configuring alpha mode")
-
-def configure_beta():
-    # Second occurrence (identical)
-    print("Enabling feature_xyz...")
-    print("Configuring beta mode")
